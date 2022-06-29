15c54f573935712171922ba64be88990938cce48
churmd <churmdaniel@gmail.com>
2022-06-29 17:10:55 +0100

    added happy path journeys for StartSession and AddMock

--- smockerclient.go
+++ smockerclient.go
@@ -1,19 +1,60 @@
 package smockerclient
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+)
+
+type Mock interface {
+	ToJson() []byte
+}
 
 type Instance struct {
-	host string
-	port int
+	url        string
+	httpClient *http.Client
 }
 
 func DefaultInstance() Instance {
 	return Instance{
-		host: "localhost",
-		port: 8081,
+		url:        "http://localhost:8081",
+		httpClient: http.DefaultClient,
 	}
 }
 
-func (i Instance) GetURL() string {
-	return fmt.Sprintf("http://%s:%d", i.host, i.port)
+func NewInstance(url string) Instance {
+	return Instance{
+		url:        url,
+		httpClient: http.DefaultClient,
+	}
+}
+
+func (i Instance) StartSession(name string) error {
+	url := i.url + "/sessions?name=" + name
+	req, err := http.NewRequest(http.MethodPost, url, nil)
+	if err != nil {
+		return fmt.Errorf("smockerclient unable to create request to start a new session %w", err)
+	}
+
+	_, err = i.httpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("smockerclient unable to send request to start a new session %w", err)
+	}
+
+	return nil
+}
+
+func (i Instance) AddMock(mock Mock) error {
+	url := i.url + "/mocks"
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(mock.ToJson()))
+	if err != nil {
+		return fmt.Errorf("smockerclient unable to create request to add a new mock %w", err)
+	}
+
+	_, err = i.httpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("smockerclient unable to send request to add a new mock %w", err)
+	}
+
+	return nil
 }
--- smockerclient_test.go
+++ smockerclient_test.go
@@ -1,16 +1,83 @@
 package smockerclient_test
 
 import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/churmd/smockerclient"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDefaultInstance(t *testing.T) {
-	expectedUrl := "http://localhost:8081"
+func TestStartSession(t *testing.T) {
+	serverCallCount := 0
+	sessionName := "my-new-session"
 
-	smockerInstance := smockerclient.DefaultInstance()
+	server := httptest.NewServer(
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				serverCallCount++
 
-	assert.Equal(t, expectedUrl, smockerInstance.GetURL())
+				assert.Equal(t, http.MethodPost, r.Method)
+				assert.Equal(t, "/sessions", r.URL.Path)
+				actualSessionName := r.URL.Query().Get("name")
+				assert.Equal(t, sessionName, actualSessionName)
+
+				resp := `{
+					"id": "1d6d264b-4d13-4e0b-a51e-e44fc80eca9f",
+					"name": "` + sessionName + `"
+				  }`
+				w.Write([]byte(resp))
+			},
+		),
+	)
+	defer server.Close()
+
+	smockerInstance := smockerclient.NewInstance(server.URL)
+	err := smockerInstance.StartSession(sessionName)
+
+	assert.NoError(t, err)
+	assert.Equal(t, 1, serverCallCount)
+}
+
+func TestAddMock(t *testing.T) {
+	serverCallCount := 0
+	expectedJson := `{"example": 1234}`
+	fakeMock := FakeMock{Json: expectedJson}
+
+	server := httptest.NewServer(
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				serverCallCount++
+
+				assert.Equal(t, http.MethodPost, r.Method)
+				assert.Equal(t, "/mocks", r.URL.Path)
+
+				body, err := ioutil.ReadAll(r.Body)
+				assert.NoError(t, err)
+				assert.JSONEq(t, expectedJson, string(body))
+
+				resp := `{
+					"message": "Mocks registered successfully"
+				  }`
+				w.Write([]byte(resp))
+			},
+		),
+	)
+	defer server.Close()
+
+	smockerInstance := smockerclient.NewInstance(server.URL)
+	err := smockerInstance.AddMock(fakeMock)
+
+	assert.NoError(t, err)
+	assert.Equal(t, 1, serverCallCount)
+}
+
+type FakeMock struct {
+	Json string
+}
+
+func (fm FakeMock) ToJson() []byte {
+	return []byte(fm.Json)
 }
